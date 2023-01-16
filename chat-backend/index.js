2a2aaef0190693cf7413b839d5c72575f350c84b
MrMisterBean <alutter03@gmail.com>
2023-01-16 09:50:36 -0500

    added temporary front-end

--- chat-backend/index.js
+++ chat-backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const http = require('http')
+const path = require('path')
 const server = http.createServer(app)
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>')
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 const PORT = 3001
