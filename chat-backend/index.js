1bd02500045ee17632e1bc778f99b970a91d0386
MrMisterBean <alutter03@gmail.com>
2023-01-16 09:57:05 -0500

    integrated socket.IO

--- chat-backend/index.js
+++ chat-backend/index.js
@@ -3,13 +3,18 @@ const app = express()
 const http = require('http')
 const path = require('path')
 const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-const PORT = 3001
+io.on('connection', (socket) => {
+  console.log('a user connected')
+})
 
+const PORT = 3001
 server.listen(PORT, () => {
   console.log(`listening on *:${PORT}`)
 })
