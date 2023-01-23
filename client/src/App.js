b8cc6d1c7465ac08ef0b0a189c6ff1fc1e3e083e
MrMisterBean <alutter03@gmail.com>
2023-01-23 17:45:24 -0500

    basic design implemented

--- client/src/App.js
+++ client/src/App.js
@@ -37,28 +37,33 @@ const App = () => {
   }
 
   return (
-    <div className='max-w-3xl m-auto'>
+    <div className='max-w-3xl p-12 py-24 h-screen m-auto'>
+
+      <div className='flex-col justify-around rounded-3xl p-4 border-black border-4 min-h-full'>
+
+        <div className='bg-white p-8 flex flex-col'>
+          {yourTexts.map((text, i) =>
+            <div className={`${text.sent ? 'text-black bg-white p-4 m-4 self-end border-2 border-black rounded-3xl' : 'text-white  bg-black p-4 m-4 self-start rounded-3xl'} `} key={i + 1}>{text.message}</div>
+          )}
+        </div>
+
+        <div className='p-24 flex'>
+          <input
+            placeholder='message...'
+            onInput={(event) => setSentText(event.target.value)}
+            className='basis-3/4 p-4 border-2 border-black rounded-l-full'
+          />
+          <button
+            onClick={sendMessage}
+            className='basis-1/4 text-xl bg-black -translate-x-10 text-white rounded-full min-w-[4.5rem]'
+          >
+            Send
+          </button>
+        </div>
+
+      </div>
+
       <div>Connected: {'' + isConnected}</div>
-
-      <div>texts array:
-        {yourTexts.map((text, i) =>
-          <div className={`${text.sent ? 'text-green-500' : 'text-red-500'}`} key={i + 1}>{text.message}</div>
-        )}
-      </div>
-
-      <div className='p-24 flex'>
-        <input
-          placeholder='message...'
-          onInput={(event) => setSentText(event.target.value)}
-          className='basis-3/4 p-4 border-2 border-black rounded-l-full'
-        />
-        <button
-          onClick={sendMessage}
-          className='basis-1/4 text-xl bg-black -translate-x-10 text-white rounded-full min-w-[4.5rem]'
-        >
-          Send
-        </button>
-      </div>
 
     </div>
   )
