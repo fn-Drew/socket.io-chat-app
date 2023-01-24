24825782e280807e6b7bfb409937e4da8a5b3a75
MrMisterBean <alutter03@gmail.com>
2023-01-24 15:50:49 -0500

    styled usernames into messages

--- client/src/App.js
+++ client/src/App.js
@@ -60,9 +60,9 @@ const App = () => {
         <div className='bg-white p-8 flex max-h-[70vh] overflow-auto flex-col'>
           {yourTexts.map((text, i) => {
             return (
-              <div key={i + 1} className={`${text.sent ? 'text-black bg-white p-4 m-4 self-end border-2 border-black rounded-3xl' : 'text-white  bg-black p-4 m-4 self-start rounded-3xl'} `}>
-                <div> {text.fromUser} </div>
-                <div key={i + 1}>{text.message}</div>
+              <div key={i + 1} className={`flex ${text.sent ? 'text-black bg-white p-4 m-4 self-end border-2 border-black rounded-3xl' : 'text-white  bg-black p-4 m-4 self-start rounded-3xl'} `}>
+                <div className='font-bold bg-white text-black self-start rounded-r-3xl p-4 -translate-x-6 -translate-y-6'> {text.fromUser} </div>
+                <div className='p-4 -translate-x-6'>{text.message}</div>
               </div>
             )
           }
