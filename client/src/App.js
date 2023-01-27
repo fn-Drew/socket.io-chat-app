287a0f3be960fc4272b5933e89a363b19ca7f834
MrMisterBean <alutter03@gmail.com>
2023-01-27 12:40:31 -0500

    updated header with hero icons

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ const App = () => {
       setIsConnected(false)
     })
     return () => {
+      console.log(isConnected)
       socket.off('connect')
       socket.off('disconnect')
       socket.off('pong')
@@ -51,21 +52,27 @@ const App = () => {
   return (
     <div className='max-w-3xl flex flex-col h-[100dvh] m-auto'>
 
-      <div className='flex justify-between bg-black text-white p-8 '>
+      <div className='flex text-xl justify-between bg-black text-white px-4 py-4 items-center'>
         <div>
-          burger
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
         </div>
         <div>
-          room: whatever
+          room: xJU7sH6aPjD4
         </div>
-        <div>
-          {'' + isConnected}
+
+        <div className='flex'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+          </svg>
         </div>
+
       </div>
 
       <div className='grow flex max-h-full overflow-auto flex-col'>
         <ConversationBox yourTexts={yourTexts} messagesEndRef={messagesEndRef} />
-        <div className='p-12' ref={messagesEndRef} />
+        <div className='p-16' ref={messagesEndRef} />
       </div>
 
       <div className='justify-center p-10 border-t-4 border-black flex'>
